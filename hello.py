9f190b5752fedd3b731b085e4cc0fde2e5c592a8
A Jain <foreverrecompin@gmail.com>
2015-12-20 20:25:41 +0530

    Added a browser information function - using the request module

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-# Route register to app's index
+# Route registered to app's index
 @app.route('/')
 def index():
     return('<h1>Hello! :></h1>')
@@ -26,6 +28,14 @@ def float_balance(balance):
 def weird_path(address):
     return("<h1>You're here: %s</h1>" %address)
 
+# Demonstrate request contexts - objects that are accessed by view
+# functions are made global temporarily to avoid cluttering view functions
+# with a lot of objects(they'd have be passed as parameters to view functions)
+@app.route('/browser')
+def browser():
+    user_agent = request.headers.get('User-Agent')
+    return ('<p>Your browser is %s</p>' % user_agent)
+
 # Ensures - web server is started only when it's directly started
 # (and not when it's imported by another script)
 if __name__ == '__main__':
@@ -37,4 +47,5 @@ http://localhost:5000/user/Joe
 http://localhost:5000/uid/123
 http://localhost:5000/float_balance/2015.007
 http://localhost:5000/weird_path/some_directory/some_file.html
+http://localhost:5000/browser
 '''
